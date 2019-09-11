da9138cff30aa425dfc241342d5e340c9317fe42
dxmvsh <dimash.live@gmail.com>
2019-09-12 00:14:10 +0600

    commit: add starting design for portrait screen

---
+++ Calculator/Calculator/Calculator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Dimash on 9/9/19.
+//  Copyright © 2019 Dimash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var display: UILabel!
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
