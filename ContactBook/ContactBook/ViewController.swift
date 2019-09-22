237580b716341aa569c6bfc7ef37165ef4bad460
dxmvsh <dimash.live@gmail.com>
2019-09-23 01:49:33 +0600

    feat(ViewController.swift): add swipe actions for rows in tableView

--- ContactBook/ContactBook/ViewController.swift
+++ ContactBook/ContactBook/ViewController.swift
@@ -45,6 +45,28 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 100
     }
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let editAction = UITableViewRowAction.init(style: .normal, title: "Edit") { (rowAction, indexPath) in
+            let editVC = self.storyboard?.instantiateViewController(withIdentifier: "EditViewController") as! EditViewController
+            editVC.dataManager = self.dataManager
+            editVC.selectedContactIndex = indexPath.row
+            self.navigationController?.pushViewController(editVC, animated: true)
+        }
+        editAction.backgroundColor = .orange
+        
+        let deleteAction = UITableViewRowAction.init(style: .normal, title: "Delete") { (rowAction, indexPath) in
+            let alert = UIAlertController.init(title: "Delete Contact?", message: "Are you sure you want to delete contact \(self.dataManager.contacts[indexPath.row].name)?", preferredStyle: .alert)
+            alert.addAction(UIAlertAction.init(title: "Yes", style: .destructive, handler: { _ in
+                self.dataManager.deleteContact(at: indexPath.row)
+                tableView.reloadData()
+            }))
+            alert.addAction(UIAlertAction.init(title: "No", style: .cancel, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+        deleteAction.backgroundColor = .red
+        
+        return [deleteAction, editAction]
+    }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let detailVC = self.storyboard?.instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
