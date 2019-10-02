d75ced93f98b7f11c6f2881af0b1771672fbf68c
dxmvsh <dimash.live@gmail.com>
2019-10-02 17:42:39 +0600

    feat: add starting files

---
+++ Browser/Browser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Browser
+//
+//  Created by Dimash on 10/2/19.
+//  Copyright © 2019 Dimash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
