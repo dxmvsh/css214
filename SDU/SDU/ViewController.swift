dd6179db69f67094c5e5c5c2c4998e8008ed3e56
dxmvsh <dimash.live@gmail.com>
2019-10-11 19:00:20 +0600

    feature: add starting files

---
+++ SDU/SDU/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SDU
+//
+//  Created by Dimash on 10/11/19.
+//  Copyright © 2019 Dimash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
