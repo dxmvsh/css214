73c76892916c05956eaefd24da0521f53c80ff20
dxmvsh <dimash.live@gmail.com>
2019-10-29 13:40:53 +0600

    feat: add NavigationController and MKMapView

---
+++ Places/Places/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Places
+//
+//  Created by Dimash on 10/29/19.
+//  Copyright © 2019 Dimash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
