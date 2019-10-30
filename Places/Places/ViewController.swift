5252c35e9d46a7dcedc84f12e68543d230258a06
dxmvsh <dimash.live@gmail.com>
2019-10-31 01:04:20 +0600

    feat: configure main view

--- Places/Places/ViewController.swift
+++ Places/Places/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var segmentedControl: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupNavigationBar()
+        segmentedControl.tintColor = .black
     }
-
+    
+    func setupNavigationBar() {
+        let blurEffectView = UIVisualEffectView(effect: UIBlurEffect(style: .light))
+        blurEffectView.frame = (self.navigationController?.navigationBar.bounds.insetBy(dx: 0, dy: -10).offsetBy(dx: 0, dy: -10))!
+        self.navigationController?.navigationBar.isTranslucent = true
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        self.navigationController?.navigationBar.addSubview(blurEffectView)
+    }
 
 }
 
